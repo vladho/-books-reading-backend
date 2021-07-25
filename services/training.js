bbce1581bb3ba1b0da20fc60948893b3509da8e8
taijayson <71400805+taijayson@users.noreply.github.com>
2021-07-25 04:24:37 +0300

    minor update response fix

--- services/training.js
+++ services/training.js
@@ -62,48 +62,6 @@ const getOne = (id) => {
 //     });
 // };
 
-// const updateOne = async (id, body) => {
-//   const { date, time, pages } = body;
-//   const { result, books, finishDate } = await Training.findById(id).populate(
-//     'books'
-//   );
-
-//   const newResult = result.find((item) => item.date === date);
-//   if (newResult) {
-//     newResult.factPages += pages;
-//     newResult.stats.push({ time, pages });
-//   } else {
-//     const totalPages = books.reduce((acc, value) => {
-//       return acc.totalPages + value.totalPages;
-//     });
-
-//     const factPages = result.reduce((acc, value) => {
-//       const dayFactPages = value.stats.reduce((acc, value) => {
-//         acc + value.pages;
-//       });
-//       return acc + dayFactPages.pages;
-//     }, 0);
-//     // console.log(factPages);
-
-//     const now = moment();
-//     // console.log(now);
-//     const formatEndDate = moment(finishDate, 'DD-MM-YYYYTHH:mm:ssZ');
-//     // console.log(formatEndDate);
-
-//     const lastDays = formatEndDate.diff(now, 'days');
-//     // console.log(lastDays);
-
-//     const plannedPages = Math.ceil((totalPages - factPages) / lastDays);
-//     // console.log(plannedPages);
-
-//     result.push({ date, plannedPages, stats: [{ time, pages }] });
-//   }
-//   // console.log(result);
-//   return await Training.findByIdAndUpdate(id, {
-//     result,
-//   }).populate('books');
-// };
-
 const updateOne = async (id, body) => {
   const { date, time, pages } = body;
   const { result, books, finishDate } = await Training.findById(id).populate(
@@ -140,7 +98,7 @@ const updateOne = async (id, body) => {
   // console.log(lastDays);
 
   const plannedPages = Math.ceil((totalPages - factPages) / lastDays);
-  console.log(plannedPages);
+  // console.log(plannedPages);
 
   const newResult = result.find((item) => item.date === date);
   // console.log(newResult?.plannedPages);
@@ -164,7 +122,15 @@ const updateOne = async (id, body) => {
       result,
     },
     { new: true }
-  ).populate('books');
+  )
+    .populate({
+      path: 'books',
+      select: '-createdAt -updatedAt',
+    })
+    .populate({
+      path: 'user',
+      select: '-token -password -createdAt -updatedAt',
+    });
 };
 
 ///////////////////backup/////////////////
