947e0f94e9203bd5f84eb1e9278ad7d6eeafb85b
Vlad <vladhopilo@gmail.com>
2021-07-27 02:38:05 +0300

    upd

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,29 +1,29 @@
-const express = require('express');
-const passport = require('passport');
+const express = require("express");
+const passport = require("passport");
 const router = express.Router();
-const { auth: ctrl } = require('../../controllers');
-const { createAccountLimiter } = require('../../helpers/rate-limit');
-const useAuth = require('../../helpers/useAuth');
-const { validateRegister, validateLogin } = require('../../validation/auth');
+const { auth: ctrl } = require("../../controllers");
+const { createAccountLimiter } = require("../../helpers/rate-limit");
+const useAuth = require("../../helpers/useAuth");
+const { validateRegister, validateLogin } = require("../../validation/auth");
 
-router.post('/register', createAccountLimiter, validateRegister, ctrl.register);
+router.post("/register", createAccountLimiter, validateRegister, ctrl.register);
 
-router.post('/login', validateLogin, ctrl.login);
+router.post("/login", validateLogin, ctrl.login);
 
-router.post('/logout', useAuth, ctrl.logout);
+router.post("/logout", useAuth, ctrl.logout);
 
-router.get('/current', useAuth, ctrl.getCurrent);
+router.get("/current", useAuth, ctrl.getCurrent);
 
 router.get(
-  '/google',
-  passport.authenticate('google', {
-    scope: ['profile', 'email'],
+  "/google",
+  passport.authenticate("google", {
+    scope: ["profile", "email"],
   })
 );
 
-router.get('/google/redirect', passport.authenticate('google'), (req, res) => {
+router.get("/google/redirect", passport.authenticate("google"), (req, res) => {
   res.redirect(
-    `http://localhost:3000/api/training?token=${req.user.token}&name=${req.user.name}&avatar=${req.user.avatar}&email=${req.user.email}`
+    `https://books-reading-backend.herokuapp.com/api/training?token=${req.user.token}&name=${req.user.name}&avatar=${req.user.avatar}&email=${req.user.email}`
   );
 });
 
