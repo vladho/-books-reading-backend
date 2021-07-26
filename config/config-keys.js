efca152d1f7643bfc9e725660b4d37fe4df263ec
Vlad <vladhopilo@gmail.com>
2021-07-27 01:38:05 +0300

    up

---
+++ config/config-keys.js
@@ -0,0 +1,19 @@
+require("dotenv").config();
+
+const { DB_HOST, GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET, COOKIE_KEY } =
+  process.env;
+
+const keys = {
+  mongodb: {
+    db: DB_HOST,
+  },
+  google: {
+    googleClientId: GOOGLE_CLIENT_ID,
+    googleClientKey: GOOGLE_CLIENT_SECRET,
+  },
+  session: {
+    cookieKey: COOKIE_KEY,
+  },
+};
+
+module.exports = keys;
